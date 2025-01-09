76d943b5fbc015bc3a8b71d07d6a0edd68ae6f7e
golamSaroar <emailsaroar@gmail.com>
2025-01-09 14:15:39 -0500

    add paraller fetching

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -17,9 +17,10 @@ export const experimental_ppr = true
 const Page = async ({ params }: {params: Promise<{id: string}>}) => {
     const id = (await params).id;
 
-    const post = await client.fetch(STARTUP_BY_ID_QUERY, {id});
-
-    const {select: editorPosts} = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {slug: "editor-picks"});
+    const [post, {select: editorPosts}] = await Promise.all([
+        await client.fetch(STARTUP_BY_ID_QUERY, {id}),
+        await client.fetch(PLAYLIST_BY_SLUG_QUERY, {slug: "editor-picks"})
+    ])
 
     if (!post) return notFound();
 
