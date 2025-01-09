8c08f21978023393a9453c8b8c564476e1da6d2d
golamSaroar <emailsaroar@gmail.com>
2025-01-09 10:22:09 -0500

    wip: add view count with ppr strategy

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -5,6 +5,9 @@ import { formatDate } from "@/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
 import markdownit from "markdown-it";
+import { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import View from "@/components/View";
 
 const md = markdownit();
 
@@ -73,7 +76,11 @@ const Page = async ({ params }: {params: Promise<{id: string}>}) => {
 
                 <hr className="divider" />
             </section>
-    </>
+
+            <Suspense fallback={<Skeleton className="view_skeleton" />}>
+                <View id={id} />
+            </Suspense>
+        </>
     )
 }
 
