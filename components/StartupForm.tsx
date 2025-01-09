538cbca44f785928aec527577287b1de928138d2
golamSaroar <emailsaroar@gmail.com>
2025-01-09 13:04:01 -0500

    save startup

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -9,11 +9,14 @@ import { Send } from "lucide-react";
 import { formSchema } from "@/lib/validation";
 import { z } from "zod";
 import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { createPitch } from "@/lib/actions";
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
   const [pitch, setPitch] = useState("");
   const { toast } = useToast();
+  const router = useRouter();
 
   const handleFormSubmit = async (prevState: any, formData: FormData) => {
     try {
@@ -26,7 +29,19 @@ const StartupForm = () => {
         };
 
         await formSchema.parseAsync(formValues);
-        console.log(formValues);
+
+        const result = await createPitch(prevState, formData, pitch);
+
+        if (result.status == "SUCCESS") {
+            toast({
+                title: "Success",
+                description: "Your startup pitch has been created successfully",
+            });
+  
+            router.push(`/startup/${result._id}`);
+        }
+
+        return result;
     } catch(error) {
         if (error instanceof z.ZodError) {
             const fieldErorrs = error.flatten().fieldErrors;
