831c4925beee435cb3218aee71eb55504d6b9d4f
golamSaroar <emailsaroar@gmail.com>
2025-01-08 21:31:29 -0500

    leverage sanity cli scripts to automatically extract types

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
@@ -33,7 +32,7 @@ export default async function Home({searchParams} : {
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType, index: number) => (
+            posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
