7e3b98633b247aa647d91de66070f8fc36c0a24c
golamSaroar <emailsaroar@gmail.com>
2025-01-09 13:55:11 -0500

    add user start ups

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -3,6 +3,11 @@ import { client } from "@/sanity/lib/client";
 import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import Image from "next/image";
+import UserStartups from "@/components/UserStartups";
+import { Suspense } from "react";
+import { StartupCardSkeleton } from "@/components/StartupCard";
+
+export const experimental_ppr = true;
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
@@ -40,7 +45,9 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
             {session?.id === id ? "Your" : "All"} Startups
           </p>
           <ul className="card_grid-sm">
-            {/* TODO: add user startups */}
+            <Suspense fallback={<StartupCardSkeleton />}>
+                <UserStartups id={id} />
+            </Suspense>
           </ul>
         </div>
       </section>
